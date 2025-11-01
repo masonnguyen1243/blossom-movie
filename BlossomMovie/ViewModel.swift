045d85423ab49a59239c91d975b47f1b2315d267
masonnguyen1243 <meowjcisgod@gmail.com>
2025-11-01 16:24:26 +0700

    fix: DataFetcher reload

--- BlossomMovie/ViewModel.swift
+++ BlossomMovie/ViewModel.swift
@@ -25,23 +25,27 @@ class ViewModel {
     
     func getTitle() async {
         homeStatus = .fetching
-        
-        do {
-            async let tMovies = try await dataFetcher.fetchTitles(for: "movie", by: "trending")
-            async let tTV = try await dataFetcher.fetchTitles(for: "tv", by: "trending")
-            async let tRMovies = try await dataFetcher.fetchTitles(for: "movie", by: "top_rated")
-            async let tRTV = try await dataFetcher.fetchTitles(for: "tv", by: "top_rated")
-            
-            trendingMovies = try await tMovies
-            trendingTV = try await tTV
-            topRatedMovies = try await tRMovies
-            topRatedTV = try await tRTV
-            
+        if trendingMovies.isEmpty {
+            do {
+                async let tMovies = try await dataFetcher.fetchTitles(for: "movie", by: "trending")
+                async let tTV = try await dataFetcher.fetchTitles(for: "tv", by: "trending")
+                async let tRMovies = try await dataFetcher.fetchTitles(for: "movie", by: "top_rated")
+                async let tRTV = try await dataFetcher.fetchTitles(for: "tv", by: "top_rated")
+                
+                trendingMovies = try await tMovies
+                trendingTV = try await tTV
+                topRatedMovies = try await tRMovies
+                topRatedTV = try await tRTV
+                
+                homeStatus = .success
+            }catch {
+                print(error)
+                homeStatus = .failure(underlyingError: error)
+            }
+        } else {
             homeStatus = .success
-        }catch {
-            print(error)
-            homeStatus = .failure(underlyingError: error)
         }
+        
     }
         
 }
